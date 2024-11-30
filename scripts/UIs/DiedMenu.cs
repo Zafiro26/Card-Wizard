a3cc22303ab1d2d51a25d110b40acbd2391ff774
Alejandro <alejandrossarabia2002@gmail.com>
2024-11-30 01:37:53 +0100

    added die state and keybinds

---
+++ scripts/UIs/DiedMenu.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class DiedMenu : Control
+{
+
+    private Button playButton;
+    private Button obtionsButton;
+    private Button exitButton;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+        playButton = GetNode<Button>("MarginContainer/VBoxContainer/PlayButton");
+        obtionsButton = GetNode<Button>("MarginContainer/VBoxContainer/ObtionsButton");
+        exitButton = GetNode<Button>("MarginContainer/VBoxContainer/ExitButton");
+        playButton.GrabFocus();
+
+        playButton.Pressed += onPlayButtonPressed;
+        obtionsButton.Pressed += onObtionsButtonPressed;
+        exitButton.Pressed += onExitButtonPressed;
+	}
+
+    private void onExitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+
+
+    private void onObtionsButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/UIs/menu.tscn");
+    }
+
+
+    private void onPlayButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/dungeon.tscn");
+    }
+}
--- scripts/states/player/DieState.cs
+++ scripts/states/player/DieState.cs
@@ -4,9 +4,6 @@ using System;
 public partial class DieState : State
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -15,6 +12,7 @@ public partial class DieState : State
 
     public override void Enter()
     {
-        base.Enter();
+        GetTree().ChangeSceneToFile("res://scenes/UIs/died_menu.tscn");
+        //QueueFree();
     }
 }
